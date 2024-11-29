a4a34189f95aed94ae892656dad70ebbc1c7d2e5
Franck Pommereau <franck.pommereau@gmail.com>
2024-11-29 14:53:55 +0100

    added memory limit

--- ecco/__main__.py
+++ ecco/__main__.py
@@ -1,14 +1,19 @@
 import argparse
+import resource
+import psutil
 
 from . import load
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--debug", action="store_true", default=False,
-                    help="run in debug mode")
-parser.add_argument("model", metavar="PATH", type=str, nargs="?", default=None,
-                    help="model to load")
-parser.add_argument("extra", type=str, nargs="*",
-                    help="langage-dependent extra arguments")
+parser.add_argument(
+    "-d", "--debug", action="store_true", default=False, help="run in debug mode"
+)
+parser.add_argument(
+    "model", metavar="PATH", type=str, nargs="?", default=None, help="model to load"
+)
+parser.add_argument(
+    "extra", type=str, nargs="*", help="langage-dependent extra arguments"
+)
 args = parser.parse_args()
 
 if args.model:
@@ -21,3 +26,6 @@ if args.model:
     else:
         for _k in _extra:
             _globals[_k] = getattr(_module, _k)
+
+limit = int(psutil.virtual_memory().total * 0.8)
+resource.setrlimit(resource.RLIMIT_DATA, (limit, limit))
