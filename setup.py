434a9a571157fdac2ecc05cda09c41dc0eb80aef
Franck Pommereau <franck.pommereau@gmail.com>
2025-03-06 10:48:23 +0100

    more robust setup

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from distutils.core import setup
 from setuptools import find_packages
@@ -8,14 +9,32 @@ from distutils.extension import Extension
 os.environ["CXX"] = "g++"
 os.environ["CC"] = "gcc"
 
+inc = [f"{os.getcwd()}/include", "include"]
+sys.path.extend(inc)
+
 setup(
     name="ecco",
     packages=find_packages(where="."),
     ext_modules=cythonize(
         [
-            Extension("ecco._ui", ["ecco/_ui.pyx"], include_dirs=["include"]),
-            Extension("ecco.lrr.lts", ["ecco/lrr/lts.pyx"], include_dirs=["include"]),
-            Extension("ecco.mrr.lts", ["ecco/mrr/lts.pyx"], include_dirs=["include"]),
+            Extension(
+                "ecco._ui",
+                ["ecco/_ui.pyx"],
+                include_path=inc,
+                include_dirs=inc,
+            ),
+            Extension(
+                "ecco.lrr.lts",
+                ["ecco/lrr/lts.pyx"],
+                include_path=inc,
+                include_dirs=inc,
+            ),
+            Extension(
+                "ecco.mrr.lts",
+                ["ecco/mrr/lts.pyx"],
+                include_path=inc,
+                include_dirs=inc,
+            ),
         ],
         language_level=3,
     ),
